002f2ea5d1f05f595c7afbd2d00e832b01aac691
Larissa Varjão <larissasilvavarjao@gmail.com>
2021-04-30 22:12:48 -0300

    feat: book post route structure

---
+++ src/books/route.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { isBookValid } = require("./validators");
+const { transformToSnakeCase } = require("../utils/cases");
+const { insertBook } = require("./repository");
+
+const router = express.Router();
+
+router.post("/books", (req, res) => {
+  const newBook = req.body;
+
+  try {
+    if (!isBookValid(newBook)) {
+      return res
+        .status(400)
+        .send({ message: "Please fill all the fields to create a book." });
+    }
+
+    const snakeCaseBook = transformToSnakeCase(newBook);
+    insertBook(snakeCaseBook);
+    res.status(201).send(snakeCaseBook);
+  } catch (e) {
+    console.error(e);
+    return res.status(400).send({ message: "Please retry later." });
+  }
+});
+
+router.get("/books", (req, res) => {});
+
+module.exports = router;
