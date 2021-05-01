86cf190c9781b888adf1146cec6de954d2d095c9
Larissa Varjão <larissasilvavarjao@gmail.com>
2021-04-30 22:11:43 -0300

    feat: add cors and update app structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const bookRoute = require("./books/route");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bookRoute);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const port = process.env.PORT || 4444;
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
