08937a16e342471ab1971d15ebd931e5255e5a20
Larissa Varjão <larissasilvavarjao@gmail.com>
2021-04-30 22:12:23 -0300

    feat: business logic on repository structure

---
+++ src/books/repository.js
@@ -0,0 +1,17 @@
+const getAllBooks = () => {
+  console.log("getAllBooks");
+};
+
+const getBookById = (id) => {
+  console.log(id);
+};
+
+const insertBook = (newBook) => {
+  console.log(newBook);
+};
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  insertBook,
+};
