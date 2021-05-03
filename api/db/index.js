5c5b073e57b9bfeb10c5a909ad68c78206c9ab86
Larissa Varjão <larissasilvavarjao@gmail.com>
2021-05-03 11:03:27 -0300

    feat: add database env variables to connection

--- api/db/index.js
+++ api/db/index.js
@@ -1,6 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
